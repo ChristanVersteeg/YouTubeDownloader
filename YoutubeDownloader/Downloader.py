05dcc4b769f2331b1682aefb4aa5850d8cb59a52
Wumpie7 <chrisjaver@gmail.com>
2023-09-07 19:27:28 +0200

    Final version of YT downloader

--- YoutubeDownloader/Downloader.py
+++ YoutubeDownloader/Downloader.py
@@ -1,10 +1,21 @@
+import os
+import re
 import pytube
+from moviepy.editor import VideoFileClip, AudioFileClip
 
 VIDEO_URL = """
 https://youtu.be/lz156C981fs
 """
+YT = pytube.YouTube(VIDEO_URL)
+EXTENSION = ".mp4"
+VIDEO = f"video{EXTENSION}"
+AUDIO = f"audio{EXTENSION}"
+TITLE = re.sub(r'[^\w\s]', '', YT.title) + EXTENSION
 
-yt = pytube.YouTube(VIDEO_URL)
-stream = yt.streams.filter(only_audio=True).first()
-stream = yt.streams.filter(only_video=True).first()
-stream.download()
+def download(filename): YT.streams.filter(only_video = filename == VIDEO, only_audio = filename == AUDIO).first().download(filename=filename)
+download(VIDEO)
+download(AUDIO)
+
+VideoFileClip(VIDEO).set_audio(AudioFileClip(AUDIO)).write_videofile(TITLE)
+os.remove(VIDEO)
+os.remove(AUDIO)
