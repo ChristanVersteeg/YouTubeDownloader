2802fa8069e2f124c15c1d4be3273198d71e0340
Wumpie7 <chrisjaver@gmail.com>
2023-09-07 18:04:02 +0200

    Split the downloaders into two, .ogg and .mp4

--- YoutubeDownloader/Downloader.py
+++ YoutubeDownloader/Downloader.py
@@ -1,17 +1,9 @@
 import pytube
-import moviepy.editor as mp
-import os
 
-VIDEO_URL = "https://youtu.be/g5DGJINVYxc?list=PLBAb9rF2kkJTppxPwzSeogOXcfHceSJC3"
-
-output_file = "disc.ogg"
+VIDEO_URL = """
+https://youtu.be/lz156C981fs
+"""
 
 yt = pytube.YouTube(VIDEO_URL)
-stream = yt.streams.filter(only_audio=True).first()
+stream = yt.streams.get_highest_resolution()
 stream.download()
-
-clip = mp.AudioFileClip(stream.default_filename)
-clip.write_audiofile(output_file)
-clip.close()
-
-os.remove(stream.default_filename)
