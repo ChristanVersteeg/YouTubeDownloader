28561287ae2e1dffbea4fcbc8c8019d4010829f4
Wumpie7 <chrisjaver@gmail.com>
2023-09-07 21:12:39 +0200

    Added an enum for the video and audio file

--- YoutubeDownloader/Downloader.py
+++ YoutubeDownloader/Downloader.py
@@ -1,5 +1,6 @@
 import os
 import re
+from enum import Enum
 import pytube
 from moviepy.editor import VideoFileClip, AudioFileClip
 
@@ -7,20 +8,22 @@ VIDEO_URL = """
 https://youtu.be/lz156C981fs
 """
 YT = pytube.YouTube(VIDEO_URL)
+
 EXTENSION = ".mp4"
 TEMP_PATH = "./Temp/"
+
 #If you want it clean you could use TITLE = fr"{re.sub(r'[^\w\s]', '', YT.title)}{EXTENSION}",
 TITLE = re.sub(r'[^\w\s]', '', YT.title) + EXTENSION 
 TEMP_TITLE = f"{TEMP_PATH}{TITLE}"
 #but that only works in Python 3.12, which is in pre-release.
-VIDEO = f"{TEMP_PATH}video{EXTENSION}"
-AUDIO = f"{TEMP_PATH}audio{EXTENSION}"
 
-def download(filename): YT.streams.filter(only_video = filename == VIDEO, only_audio = filename == AUDIO).first().download(filename=filename)
-download(VIDEO)
-download(AUDIO)
+class Type(Enum):
+    VIDEO = f"{TEMP_PATH}video{EXTENSION}"
+    AUDIO = f"{TEMP_PATH}audio{EXTENSION}"
+    
+for type in Type: YT.streams.filter(only_video = type.value == Type.VIDEO.value, only_audio = type.value == Type.AUDIO.value).first().download(filename=type.value)
 
-VideoFileClip(VIDEO).set_audio(AudioFileClip(AUDIO)).write_videofile(TEMP_TITLE)
-os.remove(VIDEO)
-os.remove(AUDIO)
+VideoFileClip(Type.VIDEO.value).set_audio(AudioFileClip(Type.AUDIO.value)).write_videofile(TEMP_TITLE)
+
+for type in Type: os.remove(type.value)
 os.rename(TEMP_TITLE, TITLE)
