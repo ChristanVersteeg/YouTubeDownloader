4b478ed3cbd6f8635c2732b6738f113ec3f5a59c
Wumpie7 <chrisjaver@gmail.com>
2023-09-08 00:40:01 +0200

    Improved the code and ignored all .ogg files

--- DiscDownloader.py
+++ DiscDownloader.py
@@ -1,17 +1,13 @@
-import pytube
-import moviepy.editor as mp
-import os
+from os import remove
+from pytube import YouTube
+from moviepy.editor import AudioFileClip
 
-VIDEO_URL = "https://youtu.be/qIzerUh9F-8"
+VIDEO_URL = """
+https://youtu.be/qIzerUh9F-8
+"""
 
-output_file = "disc.ogg"
-
-yt = pytube.YouTube(VIDEO_URL)
-stream = yt.streams.filter(only_audio=True).first()
+stream = YouTube(VIDEO_URL).streams.filter(only_audio=True).first()
 stream.download()
 
-clip = mp.AudioFileClip(stream.default_filename)
-clip.write_audiofile(output_file)
-clip.close()
-
-os.remove(stream.default_filename)
+AudioFileClip(stream.default_filename).write_audiofile("disc.ogg")
+remove(stream.default_filename)
