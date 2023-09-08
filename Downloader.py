3680d161f61b88b58ba4c0cb3295bd25b7707e89
Wumpie7 <chrisjaver@gmail.com>
2023-09-08 03:58:22 +0200

    Fixed path bug AND FINALLY FIXED TEMP FILE NOT BEING IN THE GOD DAMN TEMP FOLDER
    SERIOUSLY IT TOOK ME SO LONG TO FIND IT AND IT WAS JUST A PARAMETER I NEEDED TO CHANGE, I CHECKED THE DOCUMENTATION BUT IT WAS VAGUE. SOME TIME LATER I LOOK AGAIN AND I TEST STUFF TURNS OUT IT WAS ALWAYS THE TEMP_AUDIOFILE PARAM I WAS LOOKING FOR, BUT INTELLISENSE DOESN'T RECOGNISE IT SO REEEEEEEEEEEEEEEEEEEEEE

--- Downloader.py
+++ Downloader.py
@@ -14,20 +14,20 @@ https://youtu.be/lz156C981fs
 
 EXTENSION = ".mp4"
 TEMP_PATH = "Temp/"
-EXPORT_PATH = "Exports/"
+EXPORTS_PATH = "Exports/"
 
 def mkdir_not_exists(path): os.mkdir(path) if not os.path.exists(path) else None
 
 mkdir_not_exists(TEMP_PATH)
 windll.kernel32.SetFileAttributesW(TEMP_PATH, 0x2)
-mkdir_not_exists(EXPORT_PATH)
+mkdir_not_exists(EXPORTS_PATH)
 
 #If you want it clean you could use TITLE = fr"{sub(r'[^\w\s]', '', YT.title)}{EXTENSION}",
 TITLE = sub(r'[^\w\s]', '', YT.title) + EXTENSION 
 TEMP_TITLE = f"{TEMP_PATH}{TITLE}"
 #but that only works in Python 3.12, which is in pre-release.
 
-if os.path.exists(TITLE): os.remove(TITLE)
+if os.path.exists(f"{EXPORTS_PATH}{TITLE}"): os.remove(f"{EXPORTS_PATH}{TITLE}")
 
 class Type(Enum):
     VIDEO = f"{TEMP_PATH}video{EXTENSION}"
@@ -35,6 +35,6 @@ class Type(Enum):
     
 for type in Type: YT.streams.filter(only_video = type.value == Type.VIDEO.value, only_audio = type.value == Type.AUDIO.value).first().download(filename=type.value)
 
-VideoFileClip(Type.VIDEO.value).set_audio(AudioFileClip(Type.AUDIO.value)).write_videofile(f"{EXPORT_PATH}{TITLE}")
+VideoFileClip(Type.VIDEO.value).set_audio(AudioFileClip(Type.AUDIO.value)).write_videofile(f"{EXPORTS_PATH}{TITLE}", temp_audiofile=TEMP_TITLE)
 
 rmtree(TEMP_PATH)
